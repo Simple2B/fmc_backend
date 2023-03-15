8ed5b89d6122484dc924fcf90d8416fb0fcb5241
varvara <varvarashcherbyna7@gmail.com>
2023-03-15 18:07:48 +0200

    whoami

--- app/router/whoami.py
+++ app/router/whoami.py
@@ -9,11 +9,9 @@ whoami_router = APIRouter(prefix="/whoami", tags=["Whoami"])
 def whoami_student(current_student=Depends(get_current_student)):
     if current_student:
         return True
-    return False
 
 
 @whoami_router.get("/coach", status_code=status.HTTP_200_OK)
 def whoami_coach(current_coach=Depends(get_current_coach)):
     if current_coach:
         return True
-    return False
