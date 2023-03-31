d835def919e93a546a9cba18ff316c0f63dbf8e4
BohdanSizomin <bohdansizomin@gmail.com>
2023-03-31 10:22:57 +0300

    init

--- tasks/create_dummy_data.py
+++ tasks/create_dummy_data.py
@@ -5,14 +5,17 @@ from typing import Generator
 
 from invoke import task
 from sqlalchemy.exc import SQLAlchemyError
+from faker import Faker
 
+
+from app.config import Settings, get_settings
 from app.database import get_db, Session
 import app.model as m
 from app.logger import log
 
 db: Session = get_db().__next__()
-
-
+settings: Settings = get_settings()
+fake = Faker()
 SPORTS: list[m.SportType] = db.query(m.SportType).all()
 
 TEST_COACH_EMAIL = "coach1@gmail.com"
@@ -30,7 +33,20 @@ TEST_NEWSLETTER_NUMBER_TODAYS_NEW = 5
 TEST_NEWSLETTER_NUMBER_YESTERDAY_ACTIVE = 7
 
 
+def create_dummy_locations():
+    locations = db.query(m.Location).all()
+    if not locations:
+        location = m.Location(
+            city="London",
+            street="123",
+        )
+        db.add(location)
+    db.commit()
+    log(log.INFO, "Locations created successfully")
+
+
 def create_dummy_coach():
+    """We can authorize with this coach"""
     test_coach = db.query(m.Coach).filter_by(email=TEST_COACH_EMAIL).first()
     if not test_coach:
         test_coach = m.Coach(
@@ -56,6 +72,7 @@ def create_dummy_coach():
 
 
 def create_second_dummy_coach():
+    """We can authorize with this coach"""
     test_coach = db.query(m.Coach).filter_by(email=TEST_COACH_TWO_EMAIL).first()
     if not test_coach:
         test_coach = m.Coach(
@@ -78,6 +95,7 @@ def create_second_dummy_coach():
 
 
 def create_dummy_student():
+    """We can authorize with this student"""
     test_student = db.query(m.Student).filter_by(email=TEST_STUDENT_EMAIL).first()
     if not test_student:
         test_student = m.Student(
@@ -92,78 +110,71 @@ def create_dummy_student():
         db.commit()
 
 
-def create_footbal_coach():
-    FOOTBALL_COACH_EMAIL = "mourinho@gmail.com"
-    football_coach = db.query(m.Coach).filter_by(email=FOOTBALL_COACH_EMAIL).first()
-    if not football_coach:
-        football_coach = m.Coach(
-            first_name="Jose",
-            last_name="Mourinho",
-            username="FootballCoach",
-            email=FOOTBALL_COACH_EMAIL,
-            password="password",
-            is_verified=True,
+def create_dummy_coaches():
+    coach = db.query(m.Coach).filter_by(email=fake.email()).first()
+
+    if not coach:
+        coach = m.Coach(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            username=fake.username(),
+            profile_picture=settings.DEFAULT_AVATAR_URL,
+            about=fake.sentence(),
+            password_hash=fake.email(),
+            is_verified=random.choice([True, False]),
+            is_for_adults=random.choice([True, False]),
+            is_for_children=random.choice([True, False]),
         )
-        db.add(football_coach)
-        db.commit()
-        sport_id: int = db.query(m.SportType).first().id
+        db.add(coach)
+        db.flush()
 
-        fc_sport = m.CoachSport(coach_id=football_coach.id, sport_id=sport_id)
-        db.add(fc_sport)
-        db.commit()
-        log(log.INFO, f"Coach {football_coach} created successfully")
-    return football_coach
+    db.commit()
 
 
-def create_boxing_coach():
-    BOXING_COACH_EMAIL = "tyson@gmail.com"
-    boxing_coach = db.query(m.Coach).filter_by(email=BOXING_COACH_EMAIL).first()
-    if not boxing_coach:
-        boxing_coach = m.Coach(
-            first_name="Michael",
-            last_name="Tyson",
-            username="BoxingCoach",
-            email=BOXING_COACH_EMAIL,
-            password="password",
-            is_verified=True,
-        )
-        db.add(boxing_coach)
-        db.commit()
-        sport_id: int = db.query(m.SportType).first().id
-        bc_sport = m.CoachSport(coach_id=boxing_coach.id, sport_id=sport_id)
-        db.add(bc_sport)
-        db.commit()
-        log(log.INFO, f"Coach {boxing_coach} created successfully")
-    return db.query(m.Coach).filter_by(email=BOXING_COACH_EMAIL).first()
+# def create_footbal_coach():
+#     FOOTBALL_COACH_EMAIL = "mourinho@gmail.com"
+#     football_coach = db.query(m.Coach).filter_by(email=FOOTBALL_COACH_EMAIL).first()
+#     if not football_coach:
+#         football_coach = m.Coach(
+#             first_name="Jose",
+#             last_name="Mourinho",
+#             username="FootballCoach",
+#             email=FOOTBALL_COACH_EMAIL,
+#             password="password",
+#             is_verified=True,
+#         )
+#         db.add(football_coach)
+#         db.commit()
+#         sport_id: int = db.query(m.SportType).first().id
+
+#         fc_sport = m.CoachSport(coach_id=football_coach.id, sport_id=sport_id)
+#         db.add(fc_sport)
+#         db.commit()
+#         log(log.INFO, f"Coach {football_coach} created successfully")
+#     return football_coach
 
 
-def create_dummy_locations():
-    locations = db.query(m.Location).all()
-    if not locations:
-        locations = [
-            m.Location(
-                name="São Paulo Sport Hall",
-                city="Rua São Paulo",
-                street="123",
-            ),
-            m.Location(
-                name="Rio de Janeiro Sport Hall",
-                city="Rua São Paulo",
-                street="124",
-            ),
-            m.Location(
-                name="Rio de Janeiro Sport Hall #2",
-                city="Rua São Paulo",
-                street="125",
-            ),
-        ]
-        db.add_all(locations)
-        try:
-            db.commit()
-        except SQLAlchemyError:
-            pass
-        log(log.INFO, "Locations created successfully")
-    return locations
+# def create_boxing_coach():
+#     BOXING_COACH_EMAIL = "tyson@gmail.com"
+#     boxing_coach = db.query(m.Coach).filter_by(email=BOXING_COACH_EMAIL).first()
+#     if not boxing_coach:
+#         boxing_coach = m.Coach(
+#             first_name="Michael",
+#             last_name="Tyson",
+#             username="BoxingCoach",
+#             email=BOXING_COACH_EMAIL,
+#             password="password",
+#             is_verified=True,
+#         )
+#         db.add(boxing_coach)
+#         db.commit()
+#         sport_id: int = db.query(m.SportType).first().id
+#         bc_sport = m.CoachSport(coach_id=boxing_coach.id, sport_id=sport_id)
+#         db.add(bc_sport)
+#         db.commit()
+#         log(log.INFO, f"Coach {boxing_coach} created successfully")
+#     return db.query(m.Coach).filter_by(email=BOXING_COACH_EMAIL).first()
 
 
 def create_dummy_students():
@@ -266,10 +277,6 @@ def create_dummy_messages():
 
 
 def gen_number_emails(num_emails: int) -> Generator[str, None, None]:
-    from faker import Faker
-
-    fake = Faker()
-
     DOMAINS = ("com", "com.br", "net", "net.br", "org", "org.br", "gov", "gov.br")
 
     for _ in range(num_emails):
@@ -320,8 +327,6 @@ def dummy_data(_):
     create_dummy_student()
 
     create_dummy_locations()
-    create_footbal_coach()
-    create_boxing_coach()
     create_dummy_students()
 
     # lesson
