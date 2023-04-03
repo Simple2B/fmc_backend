9239fc8aa95a8369f3d1c5a4e0ffef97b8810c15
BohdanSizomin <bohdansizomin@gmail.com>
2023-04-03 13:27:12 +0300

    notifications fix

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,12 @@
 # flake8: noqa F401
 from .shell import shell
 from .sports import create_sports
-from .create_dummy_data import dummy_data, create_dummy_student, create_dummy_coach
+from .create_dummy_data import (
+    dummy_data,
+    create_dummy_student,
+    create_dummy_coach,
+    create_sessions,
+)
 from .newsletter import send_daily_report
 from .stripe import get_coach_subscription
 from .message import get_coaches, get_students, message_to_coach, message_to_student
