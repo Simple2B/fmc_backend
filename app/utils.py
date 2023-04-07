dc26a7f1d00a82c9dd7770a5d9e8fcfbfa60347b
Taraslysak <lysak.ipr@gmail.com>
2023-04-07 09:09:35 +0300

    fix: setup for client generation

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,10 @@
 import uuid
+from fastapi.routing import APIRoute
 
 
 def generate_uuid() -> str:
     return str(uuid.uuid4())
+
+
+def custom_generate_unique_id(route: APIRoute):
+    return f"{route.tags[0]}-{route.name}"
